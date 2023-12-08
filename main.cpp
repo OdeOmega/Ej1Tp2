98f475d1d83a2275790c5b9d94f1a802747101c1
Agus <104628858+OdeOmega@users.noreply.github.com>
2023-12-07 22:18:16 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,31 +3,42 @@
 #include <algorithm>
 
 using namespace std;
+int NO_LO_VI = 0, EMPECE_A_VER = 1, TERMINE_DE_VER = 2;
 
-void robustos(vector<vector<int>> m){
-    vector<vector<bool>> noUsado(m.size(),vector<bool>(m.size(),true));
-    vector<pair<int,int>> res;
-    for(int i = 0; i< m.size(); i++){
-        if(m[i].size() == 2){
-            pair<int,int> par1 = {min(i,m[i][0]), max(i,m[i][0])};
-            pair<int,int> par2 = {min(i,m[i][1]), max(i,m[i][1])};
-            if(noUsado[par1.first][par1.second]){
-                res.push_back(par1);
-                noUsado[par1.first][par1.second] = false;
+void dfs(int v, int p, vector<int> &estado, vector<int> &back_edges_inf_en, vector<int> &back_edges_sup_en, vector<vector<int>> &tree_edges, vector<pair<int,int>> &back_edges, vector<vector<int>> &m, vector<int> &padre) {
+    estado[v] = EMPECE_A_VER;
+    for (int u : m[v]) {
+        if (estado[u] == NO_LO_VI) {
+            padre[u] = v;
+            tree_edges[v].push_back(u);
+            dfs(u, v, estado, back_edges_inf_en, back_edges_sup_en, tree_edges, back_edges, m, padre);
+        } else if (u != p) {
+            if (estado[u] == EMPECE_A_VER){
+                back_edges_inf_en[v]++;
+                back_edges.push_back({v,u});
             }
-            if(noUsado[par2.first][par2.second]){
-                res.push_back(par2);
-                noUsado[par2.first][par2.second] = false;
-            }
+            else // estado[u] == TERMINE_DE_VER
+                back_edges_sup_en[v]++;
+
         }
     }
-    sort(res.begin(),res.end());
-    cout << res.size() << endl;
-    for(int k = 0; k< res.size();k++){
-        cout<< res[k].first << " " << res[k].second << endl;
-    }
+    estado[v] = TERMINE_DE_VER;
 }
 
+
+int cubren(int v, int p, vector<int> &memo, vector<vector<int>> &tree_edges, vector<int> &back_edges_inf_en, vector<int> &back_edges_sup_en) {
+    if (memo[v] != -1) return memo[v];
+    int res = 0;
+    for (int hijo : tree_edges[v]) {
+        if (hijo != p) {
+            res += cubren(hijo, v, memo, tree_edges, back_edges_inf_en, back_edges_sup_en);
+        }
+    }
+    res -= back_edges_sup_en[v];
+    res += back_edges_inf_en[v];
+    memo[v] = res;
+    return res;
+}
 
 int main() {
     int cantCasos, cantBases, cantEnlaces, U, V;
@@ -41,8 +52,56 @@ int main() {
             m[V].push_back(U);
         }
 
+        vector<pair<int,int>> importantes;
+        vector<int> estado(cantBases, NO_LO_VI);
+        vector<int> back_edges_inf_en(cantBases, 0);
+        vector<int> back_edges_sup_en(cantBases, 0);
+        vector<vector<int>> tree_edges(cantBases);
+        vector<pair<int,int>> back_edges;
+        vector<int> padre(cantBases);
 
-        robustos(m);
+        dfs(0,-1, estado, back_edges_inf_en, back_edges_sup_en, tree_edges, back_edges, m, padre);
+
+        vector<int> memo(cantBases, -1);
+
+        for(int k = 0; k < tree_edges.size(); k++){
+            for(int l = 0; l < tree_edges[k].size(); l++){
+                if(cubren(tree_edges[k][l], -1, memo, tree_edges, back_edges_inf_en, back_edges_sup_en) == 1){
+                    if(tree_edges[k][l] < k){
+                        importantes.push_back({tree_edges[k][l], k});
+                    }
+                    else{
+                        importantes.push_back({k, tree_edges[k][l]});
+                    }
+                }
+            }
+        }
+        if(importantes.size() == 0){
+            cout<< 0 << endl;
+        }
+        if(importantes.size() > 0){
+            for(int q = 0; q < back_edges.size(); q++){
+                bool esImportante = false;
+                int punta_inf = back_edges[q].first;
+                int inicio  = back_edges[q].first;
+                int punta_sup = back_edges[q].second;
+                while(inicio != punta_sup && !esImportante){
+                    if(cubren(inicio, -1, memo, tree_edges, back_edges_inf_en, back_edges_sup_en) == 1){
+                        esImportante = true;
+                        importantes.push_back({min(punta_inf,punta_sup), max(punta_inf,punta_sup)});
+                    }
+                    else{
+                        inicio = padre[inicio];
+                    }
+                }
+            }
+            cout << importantes.size() << endl;
+            sort(importantes.begin(), importantes.end());
+            for(int u = 0; u < importantes.size(); u++){
+                cout<< importantes[u].first << " " << importantes[u].second << endl;
+            }
+        }
     }
     return 0;
 }
+
